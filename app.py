dc6f812e3af156f86796687f1fbc4220af75effe
VladyslavSkrypets <vladskripets@gmail.com>
2021-10-02 20:19:16 +0300

    refactor for api

--- app.py
+++ app.py
@@ -1,62 +1,70 @@
-import uuid
+import requests
 from models import Urls
 from __init__ import app, db
+from utils.helpers import url_expired
+from api.api import api as api_blueprint
 from flask import render_template, request, redirect, make_response, flash, url_for
+
+
+app.register_blueprint(api_blueprint, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(render_template("404.html", body="dark"), 404)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        long_url = request.form.get("long-url")
-        url_exist = Urls.query.filter_by(url=long_url).first()
-        if url_exist:
-            url_uid = url_exist.uid
-        else:
-            url_uid = str(uuid.uuid4())[:10]
-            db.session.add(Urls(url=long_url, uid=url_uid))
-            db.session.commit()
-
-        short_url = f"{request.host_url}short/{url_uid}"
+        response = requests.post(f"{request.host_url}/api/v1/add", json={
+            "link": request.form.get("long-url"),
+            "expire_days": int(request.form.get("days_left"))
+        }, timeout=60)
+        short_url = response.json().get("short_url")
         flash("Your link converted", category='success')
         return make_response(render_template("index.html", short_url=short_url), 201)
 
     return make_response(render_template("index.html"), 200)
 
 
-@app.route("/short/<url_uid>")
+@app.route("/<url_uid>")
 def url_redirect(url_uid):
     url = Urls.query.filter_by(uid=url_uid).first()
-    if url is None:
-        flash("Incorrect url id!", category="danger")
+    if url is not None:
+        if not url_expired(url.expire_at):
+            return redirect(url.url, code=301)
+        db.session.delete(url)
+        db.session.commit()
+        flash("Link is expired!", category="danger")
         return redirect(url_for('index'))
-    return redirect(url.url, code=301)
+    flash("Incorrect link id!", category="danger")
+    return redirect(url_for('index'))
 
 
 @app.route("/all_info")
 def all_info():
-    data = Urls.query.all()
-    if data is not None:
-        info = [{"id": record.id,
-                 "url": record.url,
-                 "uid": record.uid,
-                 "created_date": record.created_date} for record in data]
-        return make_response(render_template("info.html", data=info, table_style="table"), 200)
-    flash("There is no links data", category='info')
+    response = requests.get(f"{request.host_url}/api/v1/fetch_all", timeout=60)
+    data = response.json()
+    if data:
+        return make_response(render_template("info.html", data=data, table_style="table"), 200)
+    flash("There is no links data", category='danger')
     return make_response(render_template("info.html"), 200)
 
 
 @app.route("/get_url", methods=["POST", "GET"])
 def get_url_by_id():
     if request.method == "POST":
-        uid = Urls.query.filter_by(uid=request.form.get("uid")).first()
-        if not uid:
+        response = requests.post(f"{request.host_url}api/v1/fetch_one", json={
+            "uid": request.form.get("uid").strip()
+        })
+        if response.json().get("error", ""):
             flash("There is no link for the given id or id is incorrect!", category="danger")
-            return redirect(url_for('get_url_by_id'))
+            return redirect(url_for('index'))
         flash("Found url!", category="success")
-        return make_response(render_template("get.url.html", link=uid.url), 200)
+        return make_response(render_template("get.url.html", link=response.json().get("url")), 200)
     return make_response(render_template("get.url.html"), 200)
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run()
