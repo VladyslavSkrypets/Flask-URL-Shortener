138fb222b5700cc77b23035be6ec12ce440b4a3d
VladyslavSkrypets <vladskripets@gmail.com>
2021-10-02 20:14:27 +0300

    add short url + expire to model

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-import pytz
-import datetime
 from __init__ import db
+from utils.helpers import get_date
 
 
 class Urls(db.Model):
@@ -8,12 +7,16 @@ class Urls(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text(), unique=True, nullable=False)
-    uid = db.Column(db.String(10), nullable=False)
-    created_date = db.Column(db.DateTime, default=pytz.utc.localize(datetime.datetime.utcnow()).strftime('%Y-%m-%d %H:%M:%S'))
+    short_url = db.Column(db.String(100), unique=True, nullable=False)
+    uid = db.Column(db.String(8), unique=True, nullable=False)
+    created_date = db.Column(db.DateTime, default=get_date(current=True))
+    expire_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, url: str, uid: str):
+    def __init__(self, url: str, short_url: str, uid: str, days_left: int):
         self.url = url
         self.uid = uid
+        self.short_url = short_url
+        self.expire_at = get_date(current=False, days=days_left)
 
     def __repr__(self):
         return f"<Urls {self.url}, {self.uid}>"
